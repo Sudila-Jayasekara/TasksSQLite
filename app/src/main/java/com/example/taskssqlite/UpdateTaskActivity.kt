e00ee86bdde70c66ec2360fd93ab4c6c06b45bd9
Sudila Jayasekara <91514161+Sudila-Jayasekara@users.noreply.github.com>
2024-05-12 14:20:18 +0530

    Complete Update Tasks

---
+++ app/src/main/java/com/example/taskssqlite/UpdateTaskActivity.kt
@@ -0,0 +1,40 @@
+package com.example.taskssqlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.taskssqlite.databinding.ActivityAddTaskBinding
+import com.example.taskssqlite.databinding.ActivityUpdateTaskBinding
+
+class UpdateTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUpdateTaskBinding
+    private lateinit var db: TasksDatabaseHelper
+    private var taskId: Int = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUpdateTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TasksDatabaseHelper(this)
+        taskId = intent.getIntExtra("task_id",-1)
+        if(taskId==-1){
+            finish()
+            return
+        }
+        
+        val task = db.getTaskByID(taskId)
+        binding.updateTitleEditText.setText(task.title)
+        binding.updateContentEditText.setText(task.content)
+        
+        binding.updateSaveButton.setOnClickListener{
+            val newTitle = binding.updateTitleEditText.text.toString()
+            val newContent = binding.updateContentEditText.text.toString()
+            val  updatedTask = Task(taskId,newTitle,newContent)
+            db.updateTask(updatedTask)
+            finish()
+            Toast.makeText(this, "Updated", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
